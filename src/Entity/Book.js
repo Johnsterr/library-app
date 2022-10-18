80c5e8ad6ede3eb79b12bd79bd97f3b968b37f16
Evgeniy Pashko <evgpashko92@gmail.com>
2022-10-18 12:26:33 +0700

    Create Book entity

---
+++ src/Entity/Book.js
@@ -0,0 +1,26 @@
+const {v4: uuidv4} = require("uuid");
+
+class Book {
+  constructor(
+    title,
+    description = "",
+    authors = "",
+    favorite = "",
+    fileCover = "",
+    fileName = ""
+  ) {
+    this.id = uuidv4();
+    this.title = title;
+    this.description = description;
+    this.authors = authors;
+    this.favorite = favorite;
+    this.fileCover = fileCover;
+    this.fileName = fileName;
+  }
+
+  getId() {
+    return this.id;
+  }
+}
+
+module.exports = Book;
