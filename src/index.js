85dd32963a56b8047d1b48ce819b5e1eb900712b
Evgeniy Pashko <evgpashko92@gmail.com>
2022-10-24 00:12:07 +0700

    New route, index page template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
+const nodePath = require("path");
 const express = require("express");
-// Маршруты
-const userRoutes = require("./routes/api/user.js");
-const booksRoutes = require("./routes/api/books.js");
+// Маршруты страниц
+const indexRouter = require("./routes/index.js");
+// API маршруты
+const apiUserRoutes = require("./routes/api/user.js");
+const apiBooksRoutes = require("./routes/api/books.js");
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/user", userRoutes);
-app.use("/api/books", booksRoutes);
+app.set("views", nodePath.join(__dirname, `views/`));
+app.set("view engine", "ejs");
+// Маршруты страниц
+app.use("/", indexRouter);
+// API маршруты
+app.use("/api/user", apiUserRoutes);
+app.use("/api/books", apiBooksRoutes);
 
 const PORT = process.env.APP_PORT || 3000;
 app.listen(PORT, () => {
