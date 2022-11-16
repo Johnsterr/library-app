1476847a84357fb935f1b152b6edb443f5caad60
Evgeniy Pashko <evgpashko92@gmail.com>
2022-11-17 00:25:12 +0700

    Create api/user routes

--- library-app/src/index.js
+++ library-app/src/index.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 const nodePath = require("path");
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+module.exports = passport;
+
+const {findById} = require("./Entity/User.js");
+
+// Middleware
 const errorMiddleware = require("./middleware/error.js");
 // Маршруты страниц
 const indexRouter = require("./routes/pages.js");
@@ -9,9 +17,37 @@ const indexRouter = require("./routes/pages.js");
 const apiUserRoutes = require("./routes/api/user.js");
 const apiBooksRoutes = require("./routes/api/books.js");
 
+// Utils
+const verify = require("./utils/verify.js");
+
+const options = {
+  usernameField: "username",
+  passwordField: "password",
+};
+
+passport.use("local", new LocalStrategy(options, verify));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  findById(id, (err, user) => {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+});
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(
+  session({
+    secret: "secret-key",
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("views", nodePath.join(__dirname, `views/`));
 app.set("view engine", "ejs");
--- library-app/src/routes/api/user.js
+++ library-app/src/routes/api/user.js
@@ -1,9 +1,41 @@
 const {Router} = require("express");
 const router = Router();
+const passport = require("../../index.js");
 
-router.post("/login", (req, res) => {
-  res.status(201);
-  res.json({id: 1, mail: "test@mail.ru"});
+router.get("/", (req, res) => {
+  res.render("user/index", {user: req.user});
+});
+
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
+
+router.get(
+  "/me",
+  (req, res, next) => {
+    if (!req.isAuthenticated()) return res.redirect("/api/user/login");
+    next();
+  },
+  (req, res) => {
+    res.render("user/profile", {user: req.user});
+  }
+);
+
+router.post(
+  "/login",
+  passport.authenticate("local", {failureRedirect: "/api/user/login"}),
+  (req, res) => {
+    console.log("req.user: ", req.user);
+    res.redirect("/api/user/");
+  }
+);
+
+router.post("/signup", (req, res) => {});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/api/user/");
+  });
 });
 
 module.exports = router;
