a1b8352c7ed58ea577aba47ea9b397b5a12038ef
Evgeniy Pashko <evgpashko92@gmail.com>
2022-11-17 13:38:14 +0700

    Create signup routes

--- library-app/src/routes/api/user.js
+++ library-app/src/routes/api/user.js
@@ -2,12 +2,42 @@ const {Router} = require("express");
 const router = Router();
 const passport = require("../../index.js");
 
+const {signupUser} = require("../../Entity/User.js");
+
 router.get("/", (req, res) => {
   res.render("user/index", {user: req.user});
 });
 
 router.get("/login", (req, res) => {
   res.render("user/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {failureRedirect: "/api/user/login"}),
+  (req, res) => {
+    console.log("req.user: ", req.user);
+    res.redirect("/api/user/");
+  }
+);
+
+router.get("/signup", (req, res) => {
+  res.render("user/signup");
+});
+
+router.post("/signup", (req, res) => {
+  const {username, password, name, email} = req.body;
+  const user = {
+    username,
+    password,
+    name,
+    email,
+  };
+
+  const newUser = signupUser(user);
+  req.login(newUser, (err) => {
+    res.redirect("/api/user/login");
+  });
 });
 
 router.get(
@@ -20,17 +50,6 @@ router.get(
     res.render("user/profile", {user: req.user});
   }
 );
-
-router.post(
-  "/login",
-  passport.authenticate("local", {failureRedirect: "/api/user/login"}),
-  (req, res) => {
-    console.log("req.user: ", req.user);
-    res.redirect("/api/user/");
-  }
-);
-
-router.post("/signup", (req, res) => {});
 
 router.get("/logout", (req, res) => {
   req.session.destroy(() => {
