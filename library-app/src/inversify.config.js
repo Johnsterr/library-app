05a7947190a67135e31e7a63b2be8f3c22e920d6
Evgeniy Pashko <evgpashko92@gmail.com>
2023-01-17 22:47:15 +0700

    Delete inversify compile file

--- library-app/src/inversify.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inversify_1 = require("inversify");
-var BooksRepository_1 = require("./Domains/Entities/BooksRepository/BooksRepository");
-var container = new inversify_1.Container();
-container.bind(BooksRepository_1.BooksRepository).toSelf();
-exports.default = container;
