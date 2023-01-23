2d0be810675fd230154e304c1d2aa22594643ec9
Evgeniy Pashko <evgpashko92@gmail.com>
2023-01-23 12:41:17 +0700

    Configure app module

--- library-app/src/app.module.ts
+++ library-app/src/app.module.ts
@@ -1,10 +1,17 @@
 import {Module} from "@nestjs/common";
+import {ConfigModule} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
+
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
-import {BooksModule} from "./Books/books.module";
+import {BookModule} from "./Book/book.module";
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.ME_CONFIG_MONGODB_URL!),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
