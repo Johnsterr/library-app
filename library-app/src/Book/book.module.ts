1c3aee32f7f2c92235663c0dd67e7db107d08603
Evgeniy Pashko <evgpashko92@gmail.com>
2023-01-23 12:39:56 +0700

    Create BookModule

---
+++ library-app/src/Book/book.module.ts
@@ -0,0 +1,13 @@
+import {Module} from "@nestjs/common";
+import {MongooseModule} from "@nestjs/mongoose";
+import {Book, BookSchema} from "./Schemas/book.schema";
+import {BookController} from "./book.controller";
+import {BookService} from "./book.service";
+
+@Module({
+  imports: [MongooseModule.forFeature([{name: Book.name, schema: BookSchema}])],
+  controllers: [BookController],
+  providers: [BookService],
+  exports: [BookService],
+})
+export class BookModule {}
